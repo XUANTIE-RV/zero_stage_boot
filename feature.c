060e72518412551b179645cac709e3e2bd9c2839
stephen-ji <jizhongkai.best@163.com>
2023-10-09 14:38:58 +0800

    Update feature.c
    update inital config

--- feature.c
+++ feature.c
@@ -1,11 +1,19 @@
 #include "riscv_asm.h"
 #include "riscv_encoding.h"
 
+void init_cpu(void)
+{
+	/*
+         * Invalidate & Clear IBP BTB BHT ICache & DCache
+	 */
+	csr_write(CSR_MCOR, 0x70013);
+}
+
 void setup_features(void)
 {
 	unsigned int i, cpu_type, cpu_ver;
 	unsigned long version[8];
-	unsigned long tmp;
+
 	for (i = 0; i < 8; i++)
 		version[i] = csr_read(CSR_MCPUID);
 
@@ -17,35 +25,20 @@ void setup_features(void)
 	 * you need to confirm it by your own soc design.
 	 */
 	switch (cpu_type) {
-	case 0x3: //910
-		if (cpu_ver >= 0x1080 && cpu_ver <= 0x10bf) {
-			csr_write(CSR_MCCR2, 0xe0010009);
-			csr_write(CSR_MXSTATUS, 0x638000);
-			csr_write(CSR_MHINT, 0x6e30c);
-			csr_write(CSR_MHCR, 0x11ff);
-		} else if (cpu_ver >= 0x2000 && cpu_ver <= 0xffff) {
-			csr_write(CSR_MSMPR, 0x1);
-			csr_write(CSR_MCCR2, 0xe249000b);
-			csr_write(CSR_MXSTATUS, 0x638000);
-			csr_write(CSR_MHINT, 0x31ea32c);
-			csr_write(CSR_MHINT2, 0x180);
-			csr_write(CSR_MHCR, 0x11ff);
-			tmp = csr_read(CSR_MENVCFG);
-			tmp |= 0x4000000000000000 ;
-			csr_write(CSR_MENVCFG, tmp);
-		}
-		 else if (cpu_ver >= 0x1200 && cpu_ver < 0x2000) {
+	case 0x3:
+		if (cpu_ver >= 0x1180 && cpu_ver <= 0x123f) {
 			csr_write(CSR_MSMPR, 0x1);
 			csr_write(CSR_MCCR2, 0xe249000b);
 			csr_write(CSR_MXSTATUS, 0x638000);
 			csr_write(CSR_MHINT, 0x1ee30c);
 			csr_write(CSR_MHINT2, 0x180);
 			csr_write(CSR_MHCR, 0x11ff);
-		} else if (cpu_ver >= 0x1100 && cpu_ver < 0x1200) {
+		} else if (cpu_ver >= 0x2000 && cpu_ver <= 0xffff) {
 			csr_write(CSR_MSMPR, 0x1);
-			csr_write(CSR_MCCR2, 0xe0010009);
-			csr_write(CSR_MXSTATUS, 0x638000);
-			csr_write(CSR_MHINT, 0x16e30c);
+			csr_write(CSR_MCCR2, 0xe249000b);
+			csr_write(CSR_MXSTATUS, 0x438000);
+			csr_write(CSR_MHINT, 0x31ea32c);
+			csr_write(CSR_MHINT2, 0x180);
 			csr_write(CSR_MHCR, 0x11ff);
 		} else {
 			while(1);
@@ -60,24 +53,18 @@ void setup_features(void)
 			while(1);
 		}
 		break;
-	case 0x5: //908
+	case 0x5:
 		if (cpu_ver >= 0x0000 && cpu_ver <= 0x0007) {
 			csr_write(CSR_MSMPR, 0x1);
 			csr_write(CSR_MCCR2, 0xe0420008);
 			csr_write(CSR_MXSTATUS, 0x638000);
 			csr_write(CSR_MHINT, 0x2c50c);
 			csr_write(CSR_MHCR, 0x11ff);
-		} else if (cpu_ver >= 0x00c0 && cpu_ver <= 0xffff) {
+		} else if (cpu_ver >= 0x0040 && cpu_ver <= 0xffff) {
 			csr_write(CSR_MSMPR, 0x1);
 			csr_write(CSR_MCCR2, 0xa042000a);
 			csr_write(CSR_MXSTATUS, 0x638000);
 			csr_write(CSR_MHINT, 0x21aa10c);
-			csr_write(CSR_MHCR, 0x10011ff);
-		} else if (cpu_ver >= 0x0040 && cpu_ver < 0x00c0) {
-			csr_write(CSR_MSMPR, 0x1);
-			csr_write(CSR_MCCR2, 0xa042000a);
-			csr_write(CSR_MXSTATUS, 0x638000);
-			csr_write(CSR_MHINT, 0x21ac10c);
 			csr_write(CSR_MHCR, 0x10011ff);
 		} else {
 			while(1);

1fb8f58f65324d7f6b4885d2800a28e3a53fadeb
Chen Pei <cp0613@linux.alibaba.com>
2025-02-05 15:39:47 +0800

    jump: Refactor code implementation
    1. Using fw_dynamic_info from opensbi
    2. Adjust dtb_addr and zsb_addr to be memory resident
    
    Signed-off-by: Chen Pei <cp0613@linux.alibaba.com>

--- jump.c
+++ jump.c
@@ -1,20 +1,31 @@
 #include "riscv_asm.h"
 #include "riscv_encoding.h"
+#include "fw_dynamic.h"
 
 extern unsigned long _load_start;
+static struct fw_dynamic_info info;
 
 void next_stage(void) {
-	unsigned long dtb_addr, dyn_info_addr;
-	void (*next_entry)(unsigned long arg0,unsigned long arg1,unsigned long arg2);
+	unsigned long dtb_addr, dyn_info_addr, next_addr;
+	void (*next_entry)(unsigned long arg0, unsigned long arg1, unsigned long arg2);
 
-	dtb_addr	= _load_start - (0x00100000 -0x00008000);
-	dyn_info_addr	= _load_start + (0x00008000 - 0x40);
-	next_entry	= (void (*))(_load_start - (0x04000000 - 0x00008000));
+	dtb_addr		= _load_start - (0x00100000 - 0x00008000);
+	dyn_info_addr	= (unsigned long)&info;
+	next_addr		= _load_start + 0x00008000;
+	next_entry		= (void (*))(_load_start - (0x00400000 - 0x00008000));
+
+	info.boot_hart	= 0;
+	info.magic		= FW_DYNAMIC_INFO_MAGIC_VALUE;
+	info.version	= FW_DYNAMIC_INFO_VERSION_2;
+	info.next_addr	= next_addr;
+	info.next_mode	= FW_DYNAMIC_INFO_NEXT_MODE_S;
+	info.options	= 0;
+	info.boot_hart	= 0;
 
 	/*
 	 * set $a0 = hartid
 	 * set $a1 = $dtb_addr
 	 * set $a2 = $dyn_info_addr
 	 */
-	next_entry(csr_read(CSR_MHARTID), dtb_addr ,dyn_info_addr);
+	next_entry(csr_read(CSR_MHARTID), dtb_addr, dyn_info_addr);
 }
